ce679303e25d35075ce3416fff30fc315fbbceea
blevatino <b.levatino@alumno.etec.um.edu.ar>
2025-05-11 21:35:02 -0300

    feat: Estructura básica y definición de coeficientes

---
+++ app/src/main/kotlin/org/example/App.kt
@@ -0,0 +1,13 @@
+package org.example
+
+fun main() {
+    val a: Int = 2
+    val b: Int = 1
+    val c: Int = -1
+
+    if (a == 0){
+        println("A no puede valer 0")
+    }else {
+        println("La funcion es ${a}x² + ${b}x + $c")
+    }
+}
