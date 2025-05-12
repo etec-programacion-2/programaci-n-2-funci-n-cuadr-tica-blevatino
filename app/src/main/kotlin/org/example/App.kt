2e30f9f770d31e7f7aa5fc7790cd84b5917461fb
blevatino <b.levatino@alumno.etec.um.edu.ar>
2025-05-11 21:43:05 -0300

    feat: Cálculo de valores de la función

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -1,13 +1,20 @@
 package org.example
 
 fun main() {
-    val a: Int = 2
-    val b: Int = 1
-    val c: Int = -1
+    val a: Double = 2.0
+    val b: Double = 1.0
+    val c: Double = -1.0
 
-    if (a == 0){
+    if (a == 0.0){
         println("A no puede valer 0")
     }else {
-        println("La funcion es ${a}x² + ${b}x + $c")
+        println("La funcion es f(x) = ${a}x² + ${b}x + $c")
+        cuadratica(a, b, c)
+    }
+}
+fun cuadratica(a: Double,b: Double, c: Double){
+    println("\n|   X   |   Y   |")
+    for (i in -5..5){
+        println("|  $i  |  ${a*(Math.pow(i.toDouble(),2.0))+b*i+c}  |")
     }
 }
