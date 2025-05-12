d471832c20b9862de4fc5cd921911ea2a7a9d0a4
blevatino <b.levatino@alumno.etec.um.edu.ar>
2025-05-11 22:35:18 -0300

    feat: Cálculo de raíces de la función

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -1,7 +1,7 @@
 package org.example
 
 fun main() {
-    val a: Double = 2.0
+    val a: Double = 2.1
     val b: Double = 1.0
     val c: Double = -1.0
 
@@ -10,11 +10,25 @@ fun main() {
     }else {
         println("La funcion es f(x) = ${a}x² + ${b}x + $c")
         cuadratica(a, b, c)
+        raices(a,b,c)
     }
 }
 fun cuadratica(a: Double,b: Double, c: Double){
     println("\n|   X   |   Y   |")
     for (i in -5..5){
         println("|  $i  |  ${a*(Math.pow(i.toDouble(),2.0))+b*i+c}  |")
+    }
+}
+
+fun discriminante(a: Double,b: Double, c: Double) : Double{
+    return ((Math.pow(b,2.0))-4*a*c)
+}
+
+fun raices (a: Double,b: Double, c: Double) {
+    if (discriminante(a, b, c) >= 0) {
+        println("X1 = ${String.format("%.2f",(-b + (Math.pow(discriminante(a, b, c), (1.0 / 2.0)))) / (2.0 * a))}")
+        println("X2 = ${String.format("%.2f",(-b - (Math.pow(discriminante(a, b, c), (1.0 / 2.0)))) / (2.0 * a))}")
+    }else {
+        println("No tiene raices")
     }
 }
