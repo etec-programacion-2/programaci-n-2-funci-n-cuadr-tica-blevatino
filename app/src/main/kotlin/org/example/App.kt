dbd83d4d62809665b8297e68df858dcd2d19e60c
blevatino <b.levatino@alumno.etec.um.edu.ar>
2025-05-11 22:59:43 -0300

    feat: Mejoras en presentación y formato

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -1,34 +1,48 @@
 package org.example
 
 fun main() {
-    val a: Double = 2.1
-    val b: Double = 1.0
+    // Valores de la parábola
+    val a: Double = 2.0
+    val b: Double = 4.0
     val c: Double = -1.0
 
-    if (a == 0.0){
+
+    if (a == 0.0){ // Función Incorrecta
         println("A no puede valer 0")
-    }else {
+
+    }else { // Función correcta
         println("La funcion es f(x) = ${a}x² + ${b}x + $c")
-        cuadratica(a, b, c)
-        raices(a,b,c)
+        cuadratica(a, b, c) // Tabla de valores
+        raices(a,b,c) // Valores Raices
+
+        //Tipos de Raices
+        if (discriminante(a,b,c) > 0.0){
+            println("Dos Raices Reales Diferentes")
+        }else{
+            if (discriminante(a,b,c) == 0.0){
+                println("Una Raíz Real doble")
+            }else{
+                print("")
+            }
+        }
     }
 }
-fun cuadratica(a: Double,b: Double, c: Double){
-    println("\n|   X   |   Y   |")
+fun cuadratica(a: Double,b: Double, c: Double){ //Tabla de valores
+    println("\n|    X    |    Y    |")
     for (i in -5..5){
-        println("|  $i  |  ${a*(Math.pow(i.toDouble(),2.0))+b*i+c}  |")
+        println("|  ${(String.format("%.2f",i.toDouble()))}  |  ${(String.format("%.2f",(a*(Math.pow(i.toDouble(),2.0))+b*i+c)))}  |")
     }
 }
 
-fun discriminante(a: Double,b: Double, c: Double) : Double{
+fun discriminante(a: Double,b: Double, c: Double) : Double{ // Cálculo del Discriminante
     return ((Math.pow(b,2.0))-4*a*c)
 }
 
-fun raices (a: Double,b: Double, c: Double) {
+fun raices (a: Double,b: Double, c: Double) { // Cálculo de raices
     if (discriminante(a, b, c) >= 0) {
         println("X1 = ${String.format("%.2f",(-b + (Math.pow(discriminante(a, b, c), (1.0 / 2.0)))) / (2.0 * a))}")
         println("X2 = ${String.format("%.2f",(-b - (Math.pow(discriminante(a, b, c), (1.0 / 2.0)))) / (2.0 * a))}")
     }else {
-        println("No tiene raices")
+        println("No tiene raices reales") // Si no tiene Raiz
     }
 }
